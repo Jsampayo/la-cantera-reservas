a1e951b26d7bb121b534cb15a73351ec34b552b1
Jsampayo <jsampayo61@gmail.com>
2025-11-23 18:15:15 -0500

    feat

--- src/main/java/com/la_cantera_reservas/App.java
+++ src/main/java/com/la_cantera_reservas/App.java
@@ -3,17 +3,19 @@ package com.la_cantera_reservas;
 import java.util.Scanner;
 
 import com.la_cantera_reservas.ui.MenuPrincipal;
+import com.la_cantera_reservas.ui.VistaCliente;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //Menu principal
-        MenuPrincipal.mostrarMenu(scanner);
+        // Menu principal
+        byte inicio = MenuPrincipal.menuInicio(scanner);
+        if (inicio == 1) {
+            VistaCliente.Menu_cliente(scanner);
 
-        
-
+        }
     }
 
 }
--- src/main/java/com/la_cantera_reservas/ui/MenuPrincipal.java
+++ src/main/java/com/la_cantera_reservas/ui/MenuPrincipal.java
@@ -3,7 +3,9 @@ package com.la_cantera_reservas.ui;
 import java.util.Scanner;
 
 public class MenuPrincipal {
-    public static byte mostrarMenu(Scanner input) {
+    Scanner input = new Scanner(System.in);
+
+    public static byte menuInicio(Scanner input) {
         System.out.println("\nLa Cantera Reservas");
 
         System.out.println("\n1. Acceder como Cliente\n2. Acceder como administrador\n3. Salir");
@@ -12,7 +14,8 @@ public class MenuPrincipal {
         byte opcion = input.nextByte();
         System.out.print("\n");
 
-        return opcion; 
+        return opcion;
 
     }
+
 }
